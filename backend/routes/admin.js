13553a983945a73a3110ba4e1eccf61c0970aa26
Aniket Kedari <140852884+Aniket000k@users.noreply.github.com>
2025-11-07 11:39:19 +0530

    Set email transporter to secure mode

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -9,7 +9,7 @@ const router = express.Router()
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: false, // true for 465, false for other ports
+  secure: true, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
